485a6fb520990577196f2c7ce7314c32dd37b70f
juancely <juan.cely.ramos@gmail.com>
2018-08-15 14:01:04 +0200

    updating config file

--- config/config.js
+++ config/config.js
@@ -4,17 +4,17 @@ module.exports = {
   database: {
     host    : "localhost"
   , port    : 27017
-  , dbname  : "clearsource"
+  , dbname  : "DB NAME"
   }
 
 , bullhorn: {
-    client    : "3025fef7-a040-430c-98c8-8152af082bd8"
-  , secret    : "YKYCtXYG4nwoln9ku6LBEuJ9"
-  , username  : "clearsource.rest"
-  , password  : "HubspotConnector@12"
+    client    : "YOUR CLIENT ID"
+  , secret    : "YOUR SECRET"
+  , username  : "YOUR USERNAME"
+  , password  : "YOUR PASSWORD"
   }
 
 , hubspot : {
-    api_key   : "80f0e067-bf1e-489e-a02e-b88de3215b14"
+    api_key   : "API KEY"
   }
 }
